9f291e2cbf7e1aff4916ac3f522911919d19497f
AS <a_sign@live.concordia.ca>
2018-11-11 18:43:59 -0500

    cleaned up code a bit

--- final/js/canvas.js
+++ final/js/canvas.js
@@ -9,13 +9,13 @@ window.onload = function(){
 
   //array of messages to be displayed in #3
   let messages = [
-    "0 Hello is anyone there?",
-    "1 Hacking, cloning and copying are key elements of net art.",
+    "Hello is anyone there?",
+    "Hacking, cloning and copying are key elements of net art.",
     "The ability to fabricate a new identity does not have to be constrained to be a facsimile of the physical person standing being their interface.",
-    "2 I am a vessel of identity, can you guess my race and gender?",
-    "3 I don't need a physical body anymore.",
-    "4 Packets of memories inside of other vessels suffice to convey who I am.",
-    "5 Nothing is anonymous really.",
+    "I am a vessel of identity, can you guess my race and gender?",
+    "I don't need a physical body anymore.",
+    "Packets of memories inside of other vessels suffice to convey who I am.",
+    "Nothing is anonymous really.",
     "I changed my mind, let me out!!!!",
     "Anonymity is a promised pillar of network communities, for the better or the worse.",
     "00010000100010010111010101010101001001000011100011100010111",
@@ -23,13 +23,13 @@ window.onload = function(){
   ];
 
   // array of images for changing bg in #2
-  let bgImages = new Array();
-  let bgImages[0] = new Image();
-  bgImages[0].src = 'images/bg1';
-  let bgImages[1] = new Image();
-  bgImages[1].src = 'images/bg2';
-  let bgImages[2] = new Image();
-  bgImages[2].src = 'images/bg3';
+  var bgImages = new Array();
+  bgImages[0] = new Image();
+  bgImages[0].src = 'images/bg1.jpg';
+  bgImages[1] = new Image();
+  bgImages[1].src = 'images/bg2.jpg';
+  bgImages[2] = new Image();
+  bgImages[2].src = 'images/bg3.jpg';
 
   // random for colors (and eventually images/videos)
   let random = function(min,max){
@@ -41,11 +41,11 @@ window.onload = function(){
 // RESIZE EVENT LISTENER
   window.addEventListener('resize',function(){
     // #1 change color of background -- HOW TO MAKE FULL SCREEN? ASK SANTO*
-    //wrapper.style.backgroundImage = " url(images/bg1.jpg)";
     holder.style.backgroundColor = 'rgba('+random(0,255)+','+random(0,255)+','+random(0,255)+','+random(0.8,1)+')';
 
-
     // #2 change background (images/videos)
+    canvas.style.background = bgImages[random(0,2)];
+    //holder.style.backgroundImage = "url(images/bg1.jpg)";
 
 
     // #3 cycle through array and modify text with jQuery -- GLITCHING, ASK SANTO*

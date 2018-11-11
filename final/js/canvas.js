e792d9aa0ddfbb9c7d1398d94d9d159ee3fd3467
AS <a_sign@live.concordia.ca>
2018-11-11 15:27:17 -0500

    issues, it is cycling through array BUT not updating html text :/ fts

--- final/js/canvas.js
+++ final/js/canvas.js
@@ -1,14 +1,41 @@
-console.clear();
-holder = document.getElementById('js-holder');
-console.log("hello, inside js file");
+window.onload = function(){
+  let messages = [
+    "Hello is anyone there?",
+    "Hacking, cloning and copying are key elements of net art.",
+    "The ability to fabricate a new identity does not have to be constrained to be a facsimile of the physical person standing being their interface.",
+    "I am a vessel of identity, can you guess my race and gender?",
+    "I don't need a physical body anymore.",
+    "Packets of memories inside of other vessels suffice to convey who I am.",
+    "Nothing is anonymous really."
+  ];
+  let msgLength = messages.length;
+  // let messageContents;
 
-window.addEventListener('resize',function(){
-  holder.style.background = 'rgb('+random(0,255)+','+random(0,255)+','+random(0,255)+')';
-  console.log("hi again, inside event listener");
-});
+  console.clear();
+  holder = document.getElementById('js-holder');
 
-var random = function(min,max){
-  var rand = min + Math.random()*(max+1-min);
-  rand = rand^0;
-  return rand;
+// RESIZE EVENT LISTENER
+  window.addEventListener('resize',function(){
+    // 1.) change color of background
+    holder.style.background = 'rgba('+random(0,255)+','+random(0,255)+','+random(0,255)+','+random(0.8,1)+')';
+
+
+    // 2.) cycle through array and modify text
+  for (let i=0;i<msgLength;i++) {
+    //console.log(messages[i]);
+    // messageContents += messages[i];
+    key = i % msgLength;
+    document.getElementById('p1').innerHTML = messages[key];
+    console.log(messages[key]);
+  }
+  // document.getElementById('p1').innerHTML = messageContents;
+
+
+  }); //end of event Listener
+
+  var random = function(min,max){
+    var rand = min + Math.random()*(max+1-min);
+    rand = rand^0;
+    return rand;
+  }
 }
